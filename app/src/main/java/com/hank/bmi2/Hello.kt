29a0007ea066c00f298ebe00e222b73a31ce4bef
KlinJava <alarmpinglin@gmail.com>
2024-11-14 11:52:49 +0800

    constructor

--- app/src/main/java/com/hank/bmi2/Hello.kt
+++ app/src/main/java/com/hank/bmi2/Hello.kt
@@ -5,15 +5,33 @@ class Hello {
 }
 
 fun main() {
-    val p = Person()
+    val p = Person("Jack",65.5f,1.7f)
     p.hello()
+    println(p.bmi())
+    val hank = Person("Hank",70f,1.75f)
+    val Mary = Person(1f, "Mary",66f,1.6f)
+    println("${p.name}\t${p.bmi()}")
+    println("${hank.name}\t${hank.bmi()}")
+    println(Mary.idd)
 
+//    bmiTest()
+//    println("Hello Kotlin!")
+//    dataType()
 
+}
 
-    println("Hello Kotlin!")
+private fun bmiTest() {
+    //BMI
+    val w = 65.5f
+    val h = 1.7f
+    val bmi = w / (h * h)
+    println("BMI: $bmi") // 等於 println("BMI: " + bmi)
+}
+
+private fun dataType() {
     //Byte, Short, Int, Long
-    val age:Int = 20
-    val population:Long = 9999L
+    val age: Int = 20
+    val population: Long = 9999L
     println(age.inc())
     println(age)
     var name = "Hank"
@@ -21,11 +39,9 @@ fun main() {
     name = "Jack"
     println(name)
     //Float(32), Double(64)
-    val weight:Float = 65.5f
+    val weight: Float = 65.5f
     //true, false
     val isAdult = false
     val c = 'A'
     println(c)
-
-
 }
--- app/src/main/java/com/hank/bmi2/Person.kt
+++ app/src/main/java/com/hank/bmi2/Person.kt
@@ -1,8 +1,20 @@
 package com.hank.bmi2
 
-class Person {
+class Person constructor(val name:String, val weight:Float, val height:Float) {
+    var idd : Float = 0f
+    /*    var weight:Float = 0f
+    var height:Flat = 0f*/
+    constructor(id:Float, name:String, weight:Float, height:Float):this(name, weight, height){
+        this.idd = id
+            }
+
+    fun idView() {
+        println("id: $idd")
+    }
+    fun bmi():Float {
+        return weight / (height*height)
+    }
     fun hello() {
         println("Hello")
     }
-
 }
